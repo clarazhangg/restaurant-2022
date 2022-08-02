fe5a0d44bbb23e642785704e01ffbee50cd02836
Clara Zhang <claraxixi@gmail.com>
2022-08-01 23:22:38 -0400

    Setup 2.0

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from "react";
-// import creatRoot from "react-dom";
-
-// import App from "./App";
-
-// creatRoot.render(<App />, document.getElementById("root"));
 import { createRoot } from "react-dom/client";
 
+import "./index.css";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <div>
-    <App />
-  </div>
-);
+// ReactDOM.createRoot(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
