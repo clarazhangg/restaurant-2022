efa3bfabaee1d7034664979289e437ec2f3ae294
Clara Zhang <claraxixi@gmail.com>
2022-08-16 22:16:38 -0400

    add tags on nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,10 +13,60 @@ const Navbar = () => (
     <ul className="app__navbar-links">
       <li className="p__opensans">
         <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
         <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
         <a href="#menu">Menu</a>
       </li>
+      <li className="p__opensans">
+        <a href="#award">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
     </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Login / Register
+      </a>
+      <div />
+      <a href="/" className="p__opensans">
+        Book Table
+      </a>
+    </div>
+    <div className="app__nav-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+      <div
+        className="app__navbar-smallscreen-overlay"
+        flex__center
+        slide-bottom
+      >
+        <MdOutlineRestaurantMenu
+          fontSize={27}
+          className="overlay__close"
+          onClick={() => {}}
+        />
+        <ul className="app__navbar-links">
+          <li className="p__opensans">
+            <a href="#home">Home</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#about">About</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#menu">Menu</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#award">Awards</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 
