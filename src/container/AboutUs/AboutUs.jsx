ac63e50662b417e5910d2c4ddfb86ac330f5dd9f
Clara Zhang <claraxixi@gmail.com>
2022-08-20 21:54:14 -0400

    added comments to aboutus component

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,11 +8,13 @@ const AboutUs = () => (
     className="app__aboutus app__bg flex__center section__padding"
     id="about"
   >
+    {/* G bg */}
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="G letter " />
     </div>
 
     <div className="app__aboutus-content flex__center">
+      {/* about section */}
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
@@ -25,10 +27,13 @@ const AboutUs = () => (
           Know More
         </button>
       </div>
+
+      {/* Knife img */}
       <div className="app__aboutus-content_knife flex__center">
         <img src={images.knife} alt="knife img" />
       </div>
 
+      {/* history section */}
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
