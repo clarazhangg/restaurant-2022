bbd319a81decd03514fec2e8254f38e792836a51
Clara Zhang <claraxixi@gmail.com>
2022-08-21 22:12:34 -0400

    building the menuitem jsx

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 import "./MenuItem.scss";
 
-const MenuItem = () => <div>MenuItem</div>;
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      {/* title */}
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+      </div>
+
+      {/* line btw title and price */}
+      <div className="app__menuitem-dash" />
+
+      {/* price */}
+      <div className="app__menuitem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+  </div>
+);
 
 export default MenuItem;
