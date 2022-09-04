59b9e60167701ff9694ea946e35c583da607a976
Clara Zhang <claraxixi@gmail.com>
2022-09-04 17:11:32 -0400

    error at intro jsx

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-import './Intro.css';
+import { meal } from "../../constants";
+import "./Intro.scss";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState;
+  const vidRef = React.useRef(); //will refer to the video object
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        control={false}
+        muted
+      />
+      <div className="app__overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
