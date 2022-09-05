f5d84b37754a3226ff07cfc61c5d8b70b09b7fb6
Clara Zhang <claraxixi@gmail.com>
2022-09-04 21:36:47 -0400

    laurels/awards jsx bug

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './Laurels.css';
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
+import "./Laurels.scss";
+
+const AwardCard = ({ award: { imgURL, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgURL} alt="award" />
+  </div>
+);
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    {/* content left */}
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+
+    {/* content right */}
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
