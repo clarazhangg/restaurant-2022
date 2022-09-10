1d61f0ab09d67985aea651cb5a16cb7f523445fe
Clara Zhang <claraxixi@gmail.com>
2022-09-09 23:08:35 -0400

    gallery jsx 1/3 done

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React from "react";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
 
-import './Gallery.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Gallery.scss";
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallary-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
+          Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Volutpat
+          Mattis Ipsum Turpis Elit Elit Scelerisque Egestas Mu.
+        </p>
+        <button type="button" className="custom__button">
+          Learn More
+        </button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
