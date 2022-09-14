97e049228e2d5cff0c2ebceebe93eaa61a233a93
Clara Zhang <claraxixi@gmail.com>
2022-09-14 00:24:27 -0400

    added to gallery scss and updated jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -55,14 +55,16 @@ const Gallery = () => {
             </div>
           ))}
         </div>
-        <BsArrowLeftShort
-          className="gallery__arrow-icon"
-          onClick={() => scroll("left")}
-        />
-        <BsArrowRightShort
-          className="gallery__arrow-icon"
-          onClick={() => scroll("right")}
-        />
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
+        </div>
       </div>
     </div>
   );
