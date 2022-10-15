54b276f45f8d147aaa6f0ac9331df01eb8a2f2ae
Clara Zhang <claraxixi@gmail.com>
2022-10-14 23:54:47 -0400

    edited laurels

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -13,7 +13,7 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
       <p className="p__cormorant" style={{ color: "#DCCA87" }}>
         {title}
       </p>
-      <p className="p__cormorant">{subtitle}</p>
+      <p className="p__opensans">{subtitle}</p>
     </div>
   </div>
 );
